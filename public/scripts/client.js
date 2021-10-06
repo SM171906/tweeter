371c3915a43ccb53e3bac04af0eba03114832fd3
Smiti <mishra.smiti06@gmail.com>
2021-10-06 16:27:53 -0400

    created dynamic tweet structure, added submit to form

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,93 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
+
+
+
+
+$(document).ready(() => {
+  function createTweetElement(obj) {
+    let $tweet = `<article class="tweet">
+  <header class = "article-tweet">
+    <div class = "name">
+      <img src = "${obj.user.avatars}">
+      <h5>${obj.user.name}</h5>
+    </div>
+    
+    <h5 class="username">${obj.user.handle}</h5>
+  
+  </header>
+  <div class = "tweet-body">
+  <p>${obj.content.text}</p>
+  </div>
+  <footer>
+    <span>${timeago.format(obj.created_at)}</span>
+    <div class="flags">
+      <i class="fas fa-heart"></i>
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+    
+    </div>
+  </footer>
+  </article>`
+
+
+    return $tweet;
+
+  }
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    let tweetsContent = "";
+    for (tweet of tweets) {
+      tweetsContent = tweetsContent + createTweetElement(tweet);
+    }
+    return tweetsContent;
+  }
+  renderTweets(data);
+
+  $('form').submit(function (event) {
+    event.preventDefault();
+   
+    
+});
+
+//test driver
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
